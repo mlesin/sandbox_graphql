d1a2ee23d985a7bb2b4808002072e11178d5e1e4
Mikhail Lesin <mlesin@mrpost.ru>
2020-07-23 21:16:03 +0300

    Experiments with composition api continued

--- frontend/src/components/Incrementor.vue
+++ frontend/src/components/Incrementor.vue
@@ -2,34 +2,15 @@
   <div>
     <v-btn @click="inc.actions.increment">Increment</v-btn>
     <v-btn @click="inc.actions.decrement">Decrement</v-btn>
-    <div>
-      Count is: {{ inc.state.count }}, double is: {{ inc.state.double }}
-    </div>
+    <div>Count is: {{ inc.state.count }}, double is: {{ inc.state.double }}</div>
     <div>Mouse coords are ({{ mp.x }},{{ mp.y }})</div>
-    <div
-      v-for="(item, i) in fl.list"
-      :key="i"
-      :ref="
-        el => {
-          fl.divs[i] = el;
-        }
-      "
-    >
-      {{ item }}
-    </div>
+    <div v-for="(item, i) in fl.list" :key="i" :ref="fl.fillRef">-={{ item }}=-</div>
+    <v-btn @click="fl.check">Check</v-btn>
   </div>
 </template>
 
 <script lang="ts">
-import {
-  defineComponent,
-  computed,
-  ref,
-  onMounted,
-  onUnmounted,
-  reactive,
-  onBeforeUpdate
-} from "@vue/composition-api";
+import { defineComponent, computed, ref, onMounted, onUnmounted, reactive, onBeforeUpdate, getCurrentInstance } from "@vue/composition-api";
 
 function incLogic() {
   onMounted(() => {
@@ -76,13 +57,27 @@ function useMousePosition() {
 
 function forLogic() {
   const list = reactive([1, 2, 3]);
-  const divs = ref([] as number[]);
+  const divs = ref([] as HTMLElement[]);
 
   onBeforeUpdate(() => {
     divs.value = [];
   });
 
-  return { list, divs };
+  function fillRef(i: number) {
+    return (el: HTMLElement) => {
+      console.log("fillRef called");
+      divs.value[i] = el;
+    };
+  }
+
+  function check() {
+    console.log("amount of refs:", divs.value.length);
+    list.forEach((div, i) => {
+      console.log(divs.value[i]);
+    });
+  }
+
+  return { list, divs, fillRef, check };
 }
 
 export default defineComponent({
@@ -91,6 +86,8 @@ export default defineComponent({
     const inc = incLogic();
     const mp = useMousePosition();
     const fl = forLogic();
+    const vm = getCurrentInstance();
+    console.log(vm);
 
     return {
       inc,
