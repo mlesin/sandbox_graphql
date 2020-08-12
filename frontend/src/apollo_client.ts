5cdaf8370f418d8fae6091da85469f0fb100c2c4
Mikhail Lesin <mlesin@mrpost.ru>
2020-08-12 19:06:13 +0300

    add absinthe socket

--- frontend/src/apollo_client.ts
+++ frontend/src/apollo_client.ts
@@ -1,40 +1,40 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client/core';
-// import { getMainDefinition } from '@apollo/client/utilities';
-import { WebSocketLink } from '@apollo/client/link/ws';
-// import { PhxSocketLink } from '@/apollo-absinthe/link-phx';
+import { ApolloClient, InMemoryCache, HttpLink, split } from '@apollo/client/core';
+// import { WebSocketLink } from '@apollo/client/link/ws';
+import createAbsintheSocketLink from '@absinthe/socket-apollo-link';
+import { create } from '@absinthe/socket';
+import { Socket } from 'phoenix';
+import { getMainDefinition } from '@apollo/client/utilities';
 
-const socketLink = new WebSocketLink({
-  uri: 'ws://localhost:4000/graphql',
-  options: {
-    reconnect: true,
-  },
-});
-
-// const socketLink = new PhxSocketLink({
+// const socketLink = new WebSocketLink({
 //   uri: 'ws://localhost:4000/graphql',
 //   options: {
 //     reconnect: true,
 //   },
 // });
 
+const absintheSocket = create(new Socket('ws://localhost:4000/socket'));
+const socketLink = createAbsintheSocketLink(absintheSocket);
+
 // HTTP connection to the API
-// const httpLink = new HttpLink({
-//   uri: 'http://localhost:4000/graphql',
-// });
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/graphql',
+});
 
 // The split function takes three parameters:
 //
 // * A function that's called for each operation to execute
 // * The Link to use for an operation if the function returns a "truthy" value
 // * The Link to use for an operation if the function returns a "falsy" value
-// const splitLink = split(
-//   ({ query }) => {
-//     const definition = getMainDefinition(query);
-//     return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
-//   },
-//   socketLink,
-//   httpLink,
-// );
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+  },
+  socketLink,
+  httpLink,
+);
+// just to temporarily silence eslint warning
+console.log(splitLink);
 
 // Cache implementation
 const cache = new InMemoryCache();
